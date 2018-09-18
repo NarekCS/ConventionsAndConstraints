ce5b84f68d7b0887d503b2cb827633205651acb5
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-18 16:24:55 +0400

    Conventions

---
+++ ConventionsAndConstraints/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConventionsAndConstraints.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConventionsAndConstraints.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result", new Result { Controller = nameof(HomeController), Action = nameof(Index) });
+        public IActionResult List() => View("Result", new Result { Controller = nameof(HomeController), Action = nameof(List) });
+    }
+}
---
+++ ConventionsAndConstraints/Models/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConventionsAndConstraints.Models
+{
+    public class Result
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
